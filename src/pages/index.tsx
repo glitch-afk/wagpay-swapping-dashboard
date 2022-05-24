729f377d5fb674da59e78d499258f292c16122cf
glitch-afk <rahul004@protonmail.ch>
2022-05-25 04:20:12 +0530

    feat: landing page changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import React from 'react';
 
+import Footer from '@/components/Footer';
+import First from '@/components/landing/first';
+import Second from '@/components/landing/second';
+import Support from '@/components/landing/support';
+import Third from '@/components/landing/third';
 import Navbar from '@/components/Navbar';
-import { Hero } from '@/layouts/Hero';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
@@ -17,58 +20,51 @@ const Index = () => {
       }
     >
       <Navbar />
-      <Hero>
-        <main className="relative h-fit pt-24 pb-20 sm:pb-0 lg:mb-0">
-          <div className="mx-auto max-w-7xl">
-            <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-              <div className="flex flex-col items-center justify-center space-y-8 sm:px-6 sm:text-center md:mx-auto md:max-w-5xl md:items-start lg:col-span-6 lg:flex lg:items-center lg:text-left">
-                <div className="flex w-full flex-col items-center justify-center space-y-1 md:space-y-2 lg:items-start">
-                  <span className="text-5xl font-extrabold text-wagpay-dark drop-shadow-lg sm:text-7xl">
-                    Frictionless
-                  </span>
-                  <span className="bg-gradient-to-br from-orange-700 to-yellow-400 bg-clip-text text-5xl font-extrabold text-transparent drop-shadow-lg sm:text-7xl">
-                    Crosschain
-                  </span>
-                  <span className="text-5xl font-extrabold text-wagpay-dark drop-shadow-lg sm:text-7xl">
-                    Swapping
-                  </span>
-                </div>
-                <div className="flex w-full items-center justify-center text-center text-lg lg:justify-start">
-                  Helping you choose the best bridge for your swap
-                </div>
-                <div className="flex w-full items-center justify-center space-x-2 md:space-x-6 lg:justify-start">
-                  <Link href="/">
-                    <a className="rounded-full bg-wagpay-dark py-3 px-6 text-white drop-shadow-lg">
-                      Coming Soon
-                    </a>
-                  </Link>
-                  {/* add waitlist form url here */}
-                  <a
-                    className="rounded-full border border-wagpay-dark bg-white py-3 px-6 text-wagpay-dark drop-shadow-lg"
-                    href="#"
-                  >
-                    Join waitlist
-                  </a>
-                </div>
-              </div>
-              <div className="mt-16 w-full sm:mt-24 lg:col-span-6 lg:mt-0">
-                <div className="sm:mx-auto sm:w-full sm:max-w-md sm:overflow-hidden sm:rounded-lg">
-                  <Image
-                    src="/images/group.svg"
-                    alt="landing_page_image"
-                    priority
-                    width={520}
-                    height={520}
-                    placeholder="blur"
-                    blurDataURL="/images/group.svg"
-                    layout="responsive"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </Hero>
+      <main className="relative h-fit bg-wagpay-dark pt-24 pb-20 sm:pb-0 lg:mb-0">
+        <div className="mx-auto max-w-7xl sm:mb-28">
+          <First />
+        </div>
+        <div className="relative w-full bg-wagpay-dark">
+          <svg
+            width="1512"
+            height="1175"
+            viewBox="0 0 1512 1175"
+            fill="none"
+            className="absolute inset-x-0 hidden w-full xl:block"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <ellipse
+              cx="756"
+              cy="587.5"
+              rx="587.5"
+              ry="982"
+              transform="rotate(-90 756 587.5)"
+              fill="url(#paint0_linear_232_385)"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_232_385"
+                x1="187.5"
+                y1="660.999"
+                x2="1108.68"
+                y2="731.336"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#B1D9FF" stopOpacity="0" />
+                <stop offset="1" stopColor="#E8E9FF" stopOpacity="0.04" />
+              </linearGradient>
+            </defs>
+          </svg>
+          <Second />
+        </div>
+        <div className="mx-auto mb-28 max-w-7xl text-white">
+          <Third />
+        </div>
+        <div className="mx-auto max-w-7xl text-white">
+          <Support />
+        </div>
+        <Footer />
+      </main>
     </Main>
   );
 };
